a780a94659fd0cb8ff84a8f0827eaa5139179c43
Janna02 <dendy2@yandex.ru>
2018-03-17 20:45:29 +0300

    Калькулятор

---
+++ src/Calc.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Calc {
+    public static void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое дробное число:");//пример 1.23
+        double a = scanner.nextDouble();
+        System.out.println("Введите второе дробное число:");//пример 1.23
+        double b = scanner.nextDouble();
+        System.out.println("Выберите операцию: +, -, *, /");
+        String oper = scanner.next();
+        double result = 0.0;
+        switch (oper) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a + b;
+                break;
+            default:
+                System.out.println("Вы ввели некорректную операцию");
+        }
+        /*
+         Вывод результата.
+         */
+        System.out.printf("%.4f", result);
+    }
+    }
+
