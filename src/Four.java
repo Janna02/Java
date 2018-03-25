4bb29c32ea8b1d8470af32b0be7fe7787a8c93ac
Janna02 <dendy2@yandex.ru>
2018-03-25 14:45:27 +0300

    Delete Four.java

--- src/Four.java
+++
@@ -1,9 +0,0 @@
-public class Four {
-    public static void main (String [] args){
-        int[] array = new int[20];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ((int)(Math.random() * 20) - 10);
-            System.out.println(array[i]);
-        }
-    }
-}
